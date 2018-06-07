1e9476769859bd3d66405febcaa8e410643ffd6c
Frank Li <li.26.frank@gmail.com>
2018-06-07 02:04:30 -0400

    Google OAuth, passport, and session configuration

---
+++ routes/authRoutes.js
@@ -0,0 +1,14 @@
+const passport = require('passport');
+
+module.exports = app => {
+  app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile'] })
+  );
+
+  app.get('/auth/google/callback',
+    passport.authenticate('google'), 
+    (req, res) => {
+      res.send('ok');
+    }
+  );
+};
---
+++ services/passport.js
@@ -0,0 +1,34 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+
+const keys = require('../config/keys');
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id); 
+  done(null, user);
+});
+
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientId,
+    clientSecret: keys.googleSecretKey,
+    callbackURL: '/auth/google/callback',
+    proxy: true
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    let user = await User.findOne({ googleId: profile.id });
+
+    if (!user) {
+      user = await new User({ googleId: profile.id }).save();
+    }
+
+    done(null, user);
+  }
+));
+
