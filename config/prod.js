1dba7c1cbd7b09357c5419cc6705d690972f920b
Frank Li <li.26.frank@gmail.com>
2018-07-04 01:42:04 -0400

    configured dev and prod keys

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
+  googleSecretKey: process.env.GOOGLE_SECRET_KEY,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+  sendgridKey: process.env.SENDGRID_KEY,
+  twilioSID: process.env.TWILIO_SID,
+  twilioToken: process.env.TWILIO_TOKEN,
+  twilioNumber: process.env.TWILIO_NUMBER
+};
